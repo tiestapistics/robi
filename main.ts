32a1dafe5b7f3ab8bd0bab7210442e09ca36023a
tieStAPistics <49621630+tiestapistics@users.noreply.github.com>
2022-03-26 16:35:32 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,88 +1,3 @@
-function showBatteryLevel(): void {
-    brick.clearScreen();
-    
-    const batteryLevel: number = brick.batteryLevel();
-    brick.showString('Battery Level:', 2)
-    brick.showString('' + batteryLevel, 3)
-
-    const voltage = brick.batteryInfo(BatteryProperty.Voltage);
-    brick.showString('Voltage:' + Math.roundDecimal(voltage) + ' V', 5)
-
-    const current = brick.batteryInfo(BatteryProperty.Current);
-    brick.showString('Current:' + Math.roundDecimal(current) + ' mA', 6)
-}
-
-function showSerialNumber(): void {
-    brick.clearScreen();
-
-    const firmwareVersion: string = control.deviceFirmwareVersion();
-    brick.showString('Firmware Version:', 2)
-    brick.showString('' + firmwareVersion, 3)
-
-    const serialNumber: number = control.deviceSerialNumber();
-    brick.showString('Device Serial Number:', 4)
-    brick.showString('' + serialNumber, 5)
-
-    /*
-    const longSerialNumber: string = control.deviceLongSerialNumber().toString();
-    brick.showString('Long Device Serial Number:', 6)
-    brick.showString('' + longSerialNumber, 7)
-
-    const dalVersion: string = control.deviceDalVersion();
-    brick.showString('DAL Version:', 8)
-    brick.showString('' + dalVersion, 9)
-    */
-}
-
-function showGyro(): void {
-    const s = 10;
-    const update = 200;
-
-    for (let index = 0; index < s * 1000 / update; index++) {
-        brick.clearScreen();
-        brick.showString('Gyro: ' + index, 2)
-        brick.showString('' + hardware.readGyroAngle(), 3)
-        pause(update);
-    }
-    brick.clearScreen();
-}
-
-function showColor(): void {
-    const s = 10;
-    const update = 200;
-
-    for (let index = 0; index < s * 1000 / update; index++) {
-        brick.clearScreen();
-        brick.showString('Color: ' + index, 2)
-        brick.showString('' + hardware.readReflectedLight(), 3)
-        pause(update);
-    }
-    brick.clearScreen();
-}
-
-function showPorts(): void {
-    robi.setLive(false);
-    brick.showPorts()
-}
-
-function TEST1(): void {
-    robi.actionMove(40)
-}
-
-function TEST2(): void {
-    robi.actionMove(-40)
-}
-
-function TEST3(): void {
-    robi.actionRotate(180, 0, -100)
-}
-
-function TEST4(): void {
-    robi.actionRotate(-180)
-}
-
-// ---
-
 let count = 1;
 function PAUSE(): void {
     brick.clearScreen()
@@ -107,7 +22,6 @@ function MotorA(action: string, n?: number, s?: string): void {
 
     if (s != 'nowait') {
         motors.mediumA.pauseUntilReady();
-        //motors.mediumA.pauseUntilStalled();
     }
 }
 
@@ -128,7 +42,6 @@ function MotorD(action: string, n?: number, s?: string): void {
 
     if (s != 'nowait') {
         motors.mediumD.pauseUntilReady();
-        //motors.mediumD.pauseUntilStalled();
     }
 }
 
@@ -198,119 +111,6 @@ function programHubschrauber(): void {
     robi.startProgram();
 }
 
-function programDavid(): void {
-    robi.actionClean();
-    robi.actionOnExit(MotorA_stop);
-    robi.actionCallback("reset", MotorA_reset);
-    robi.actionCallback("hoch", MotorA, 180, 'nowait');
-
-    robi.actionFollowGyro(0, 110);
-    robi.actionStop();
-
-    robi.actionCallback("runter", MotorA, -90, 'nowait');
-    robi.actionFollowGyro(-70, 20);
-    robi.actionStop();
-
-    robi.actionCallback("runter", MotorA, -90, 'nowait');
-    robi.actionFollowGyro(0, 20);
-    robi.actionStop();
-
-    robi.startProgram();
-}
-
-function program1(): void {
-    robi.actionClean();
-    robi.actionMove(40)
-    robi.actionRotate(-45)
-    robi.actionMove(70)
-    robi.actionRotate(35)
-    robi.actionMove(70)
-    robi.startProgram();
-}
-
-function program2(): void {
-    robi.actionClean();
-    robi.actionFollowGyro(45, 40)
-    robi.actionFollowGyro(0, 70)
-    robi.actionFollowGyro(35, 70)
-    robi.startProgram();
-}
-
-function program3(): void {
-    robi.actionClean();
-    robi.actionFollowColor(160, robi.FollowLineType.right)
-    robi.startProgram();
-}
-
-function program4(): void {
-    robi.actionClean();
-    robi.actionFollowGyro(0, 50)
-    robi.actionFollowGyro(90, 50)
-    robi.actionFollowGyro(90, -50)
-    robi.actionFollowGyro(0, -50)
-
-    robi.actionFollowGyro(-90, 0)
-    robi.actionFollowGyro(0, 0)
-    robi.startProgram();
-}
-
-// ---
-
-let TESThardware_check_a: number;
-let TESThardware_check_b: number;
-function TESThardware_check(action: string, n?: number) {
-    switch (action) {
-        case 'pause':
-            pause(3 * 1000);
-            break;
-
-        case 'motor1':
-            TESThardware_check_a = hardware.motorConfig.readMotorPosition()[M1];
-            break;
-        case 'motor2':
-            TESThardware_check_b = hardware.motorConfig.readMotorPosition()[M1];
-            Test.assertTrue((Math.abs(n - (TESThardware_check_b - TESThardware_check_a)) < 3), 'motorCheck');
-            break;
-
-        case 'gyro1':
-            TESThardware_check_a = hardware.readGyroAngle();
-            break;
-        case 'gyro2':
-            TESThardware_check_b = hardware.readGyroAngle();
-            Test.assertTrue((Math.abs(n - (TESThardware_check_b - TESThardware_check_a)) < 10), 'gyroCheck');
-            break;
-    }
-}
-function TESThardware(): void {
-    robi.actionClean();
-
-    robi.actionCallback('motor1', TESThardware_check);
-    robi.actionMove(10);
-    robi.actionCallback('motor2', TESThardware_check, 10);
-    robi.actionCallback('pause', TESThardware_check);
-
-    robi.actionCallback('motor1', TESThardware_check);
-    robi.actionMove(-10);
-    robi.actionCallback('motor2', TESThardware_check, -10);
-    robi.actionCallback('pause', TESThardware_check);
-
-    // ---
-
-    robi.actionCallback('gyro1', TESThardware_check);
-    robi.actionRotate(90);
-    robi.actionCallback('gyro2', TESThardware_check, 90);
-    robi.actionCallback('pause', TESThardware_check);
-
-    robi.actionCallback('gyro1', TESThardware_check);
-    robi.actionRotate(-90);
-    robi.actionCallback('gyro2', TESThardware_check, -90);
-    robi.actionCallback('pause', TESThardware_check);
-
-    // ---
-
-    robi.startProgram();
-}
-
 // ---
 
 function startup() {
@@ -341,36 +141,7 @@ function startup() {
     menu.newMenu('', 'Jana und Judith', programJJ);
     menu.newMenu('', 'LKWs', programLKW);
     menu.newMenu('', 'Hubschrauber', programHubschrauber);
-    menu.newMenu('', 'David_2022-03-17', programDavid);
-//    menu.newMenu('', 'program1', program1);
-//    menu.newMenu('', 'program2', program2);
-//    menu.newMenu('', 'program3', program3);
-//    menu.newMenu('', 'program4', program4);
-    menu.newMenu('', 'TESTS', null, menuTests);
-    menu.newMenu('', 'TOOLS', null, menuTools);
     menu.newMenu('', 'releaseBreaks', hardware.releaseBreaks);
-    menu.newMenu('', 'EXIT', brick.exitProgram);
-
-    new menu.MenuType(menuTests);
-    menu.newMenu(menuTests, 'ZURUECK', null, '');
-    menu.newMenu(menuTests, 'LOGGER', null, 'logger');
-    menu.newMenu(menuTests, 'TESTnormAngle', Helper.TESTnormAngle);
-    menu.newMenu(menuTests, 'TESTspeed', Helper.TESTspeed);
-    menu.newMenu(menuTests, 'TESThardware', TESThardware);
-    menu.newMenu(menuTests, 'showGyro', showGyro);
-    menu.newMenu(menuTests, 'showColor', showColor);
-    menu.newMenu(menuTests, 'show Ports', showPorts);
-    menu.newMenu(menuTests, 'show Battery Level', showBatteryLevel);
-    menu.newMenu(menuTests, 'show Serial Number', showSerialNumber);
-
-    new menu.MenuType(menuTools);
-    menu.newMenu(menuTools, 'ZURUECK', null, '');
-    menu.newMenu(menuTools, 'LOGGER', null, 'logger');
-    menu.newMenu(menuTools, 'TEST1', TEST1);
-    menu.newMenu(menuTools, 'TEST2', TEST2);
-    menu.newMenu(menuTools, 'TEST3', TEST3);
-    menu.newMenu(menuTools, 'TEST4', TEST4);
-    menu.newMenu(menuTools, 'startProgram', robi.startProgram);
 
     brick.clearScreen()
     menu.setMenu('');
